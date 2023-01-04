d8848a4ee50e4a866e47b02d489e3a17d6c2183f
namuunbaigal <namuunbaigali266@gmail.com>
2023-01-04 17:51:11 +0800

    d

--- fetch/yucbsnydgi/yuuu.js
+++ fetch/yucbsnydgi/yuuu.js
@@ -1,25 +1,33 @@
-fetch("https://randomuser.me/api", {
-  method: "get",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(card),
-});
-
-const getCard = function (card) {
-  return ` 
-  <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+const root = document.getElementById("root");
+const getByCard = async () => {
+  fetch("https://randomuser.me/api", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      root.innerHTML = `<div class="card" style="width: 18rem;">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOobN2lNm_JXxMhUhF3Uxdehn46t7RfNsRSQ&usqp=CAU" class="card-img-top" alt="...">
   <div class="card-body">
-  <img
-                src="${card.imageUrl}"
-                class="cars-img-top"
-                alt=""
-              />
-    <p class="card-text">${card.phone}</p>
-    <p class="card-text">${card.location}</p>
-    <p class="card-text">${card.country}</p>
-  </div> s
-</div>
-`;
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>`;
+    });
+  //   .then((res) => res.json())
+  //       .then((res) => {
+  //         alert("amjiltta ustgalaa");
+  //         getCarsHtml();
+  //       })
+  //       .catch((res) => {
+  //         console.warn(err);
+  //       });
 };
+// const getCard = async function (card) {
+//   await console.log(getByCard());
+//   return;
+// };
+getByCard();
+// root.innerHTML = getByCard();
